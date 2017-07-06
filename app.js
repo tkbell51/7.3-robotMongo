bb389b689268d64efbe61059012b93557595d69d
TKB <TKB@TIMOTHYs-MacBook-Pro.local>
2017-07-06 00:36:52 -0400

    working and unemployed but

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const express = require('express');
-const app = express();
-const MongoClient = require('mongodb').MongoClient;
 const mustacheExpress = require('mustache-express');
-
-
+const datascript = require('./datascript');
+const roboController = require('./controllers/roboController');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
 app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
-app.set('views', './views');
 app.set('view engine', 'mustache');
+app.set('views', './views');
 
-app.use (function(req, res, next){
-  MongoClient.client('mongodb://locoalhost:27017/timKBdb', (error, db)=>{
+
+
+
+app.use(function(req, res, next){
+  MongoClient.connect('mongodb://localhost:27017/timRobots_db', (errors, db)=>{
     req.db = db;
     next();
   })
 })
+
+
 app.get('/directory', roboController.directory);
 app.get('/directory/:id', roboController.profile);
-app.get('/directory/working', roboController.working);
 app.get('/directory/unemployed', roboController.unemployed);
+app.get('/directory/working', roboController.working)
+// app.get('/directory/:id', roboController.profile);
+// app.get('/directory/working', roboController.working);
+// app.get('/directory/unemployed', roboController.unemployed);
 
-app.listen(3000, function(){
-  console.log('This is my directory!!');
-});
+app.listen(3000);
--- controllers/roboController.js
+++ controllers/roboController.js
@@ -1,22 +1,49 @@
-const Data = require('./datascript');
+const MongoClient = require('mongodb').MongoClient;
+const data = require('../data');
 
 module.exports = {
-  directory: function(req, res, next){
-    const context = {};
-    MongoClient.connect('mondgodb://localhost:27017/timKBdb', (errors. db)=>{
-      const robots = req.db.collection('robots');
-      robots.find({}).toArray((error, results)=>{
-        context.model = results;
-        res.render('directory', context);
-      })
+
+  directory: function(req, res){
+    var context = {};
+
+    var col = req.db.collection('robots');
+
+    col.find({}).toArray((error, results)=>{
+      console.log(results);
+      context.model = results;
+      res.render('directory', context);
     })
+
 
   },
 
   profile: function(req, res){
-
+    var context ={};
     var user = req.params.id;
-    const profile = Data.findOne(user);
-    res.render('profile', {model: profile});
+    user = Number(user);
+    var col = req.db.collection('robots')
+    col.find({"id": user}).toArray((error, results)=>{
+      context.model = results
+      res.render('profile', context);
+    });
+
+  },
+  unemployed: function(req, res){
+    var context = {};
+    var col = req.db.collection('robots')
+    col.find({'job': null}).toArray((error, results)=>{
+      console.log("unemployeeeeeeeeed", results);
+      context.model = results
+      res.render('unemployed', context);
+    })
+
+  },
+  working: function(req, res){
+    var context = {};
+    var col = req.db.collection('robots')
+    col.find({'company': {$ne: null}}).toArray((error, results)=>{
+      context.model = results
+      res.render('working', context);
+    })
   }
 };
